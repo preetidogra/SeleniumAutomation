b93c2eb5e79d83e73baa3e745cf0078e92e21ce8
HP <HP@DESKTOP-N69L4IA>
2022-11-25 10:21:32 +0100

    Add project files.

---
+++ SeleniumAutomation/Login.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation
+{
+    internal class Login
+    {
+
+        IWebDriver driver = new ChromeDriver();
+    }
+}
